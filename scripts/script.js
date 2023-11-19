0844b7fe78ddd5b755a6d040ae174bffae831243
scheeseb <samuel.cheeseboro@gmail.com>
2023-11-18 20:45:27 -0600

    Created a function library for the script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,29 @@
-let pageTitle = document.querySelector("head > title");
-let titleString = pageTitle.innerHTML;
-let titleIsQualifying = titleString.includes("Qualifying");
-let titleIsHighlights = titleString.includes("Highlights");
+function identifyPage(){
+    let pageTitle = document.querySelector("head > title");
+    let titleString = pageTitle.textContent;
+    let titleIsQualifying = titleString.includes("Qualifying");
+    let titleIsHighlights = titleString.includes("Highlights");
+}
+function getTimeStamp(){
+    let timeLabel = document.querySelector("#bmpui-id-57");
+    let timeString = timeLabel.textContent;
+    console.log(timeString)
+};
+function clickPlay(){
+    let playStatusLabel = document.querySelector("#bmpui-id-67");
+    playStatusLabel.click();
+}
+function clickForward(timeDifference){
+    let tenSecForward = document.querySelector("#bmpui-id-2");
+    tenSecForward.click();
 
-console.log(pageTitle)
-console.log(titleString)
-console.log(titleIsQualifying)
-console.log(titleIsHighlights)
-
-function clickForward() {};
-function clickBackward() {}
+    let playStatusLabel = document.querySelector("#bmpui-id-67");
+    let playStatus = playStatusLabel.ariaLabel;
+    // Pauses the Video if Playing
+    if(playStatus === "Pause"){
+        setTimeout(function() {
+            clickPlay();
+          }, 5000);
+    }
+};
+function clickBackward(){}
